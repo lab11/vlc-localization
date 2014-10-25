9344dec0c69d4554196672f57a5d8fd442d8a6b0
Pat Pannuto <pat.pannuto@gmail.com>
2014-10-25 01:39:39 -0400

    Wire in iphone5 camera

--- processing/phones/__init__.py
+++ processing/phones/__init__.py
@@ -1,2 +1,2 @@
-__all__ = ['lumia_1020']
+__all__ = ['lumia_1020', 'iphone5']
 from . import *
---
+++ processing/phones/iphone5.py
@@ -0,0 +1,5 @@
+from .cameras import iphone5_back as back
+#from .cameras import iphone5_front as front
+
+#cameras = [back, front]
+cameras = [back, ]
