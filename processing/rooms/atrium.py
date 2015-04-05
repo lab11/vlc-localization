6a32394986352a64e3946c3c277bfe593c94814e
Pat Pannuto <pat.pannuto@gmail.com>
2015-04-05 03:26:21 -0400

    Add forgotten file

---
+++ processing/rooms/atrium.py
@@ -0,0 +1,14 @@
+
+origin = 'center'
+
+transmitters = {
+		# Chandelier 0
+
+		# Measured
+		4000 : (( 3.751, 4.684, 3.382),),		# Bulb 5
+		4500 : (( 6.159, 4.755, 3.500),),		# Bule e
+		5000 : (( 2.142, 6.239, 3.400),),		# Bulb 2
+		5500 : (( 4.005, 5.877, 3.430),),		# Bulb 7
+
+		# From Calibration
+		}
